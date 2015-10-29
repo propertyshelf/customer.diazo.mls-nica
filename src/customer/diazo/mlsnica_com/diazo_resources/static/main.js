8f6f82638a42f5782321082e6eeb5191e57164dc
Kantanart <kantanat@propertyshelf.com>
2015-10-29 17:38:33 +0700

    Update MLS nica theme
    - Style header (responsive)
    - Style Doormat/colophone (responsive)
    - Add Rules , clean html
    - Create center logo
    - Change font family and add google font
    - Style portlet

--- src/customer/diazo/mlsnica_com/diazo_resources/static/main.js
+++ src/customer/diazo/mlsnica_com/diazo_resources/static/main.js
@@ -1,5 +1,9 @@
-jQuery(function(jq) {
-
-  // Add your theme JS code here.
-
+function center_logo_url() {
+    link = $("#portal-logo").attr("href");
+    $(".center-logo a").attr("href" , link);
+}
+$(document).ready(function() {
+	if($(".center-logo").length >0){
+		center_logo_url();
+	}
 });
