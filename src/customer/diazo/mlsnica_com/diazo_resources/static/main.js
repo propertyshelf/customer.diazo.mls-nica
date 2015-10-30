2722561ace350d44a391c7fcf9ccfc69592b446b
Kantanart <kantanat@propertyshelf.com>
2015-10-30 14:25:36 +0700

    Update css , manifest , delete js
    - Manifest.cfg
    - Style Portlet Quick Search
    - Add images for portlet (Arrow white >)
    - Improve Listing summary

--- src/customer/diazo/mlsnica_com/diazo_resources/static/main.js
+++
@@ -1,9 +0,0 @@
-function center_logo_url() {
-    link = $("#portal-logo").attr("href");
-    $(".center-logo a").attr("href" , link);
-}
-$(document).ready(function() {
-	if($(".center-logo").length >0){
-		center_logo_url();
-	}
-});
